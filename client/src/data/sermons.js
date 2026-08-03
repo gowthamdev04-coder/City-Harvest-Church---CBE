33b06c9c9dc369893974bd7c99aa65b57bab17e2
Gowtham <gowthamkrish78@gmail.com>
2026-08-03 23:23:36 +0530

    Design Updates

--- client/src/data/sermons.js
+++ client/src/data/sermons.js
@@ -6,17 +6,17 @@ export const featuredSermon = {
   speaker: 'Bro. Jublin Joseph',
   date: 'July 19, 2026',
   type: 'Revival Night',
-  thumbnail: 'https://i.ytimg.com/vi/vSGA7lXByTU/hqdefault.jpg',
+  thumbnail: 'https://i.ytimg.com/vi/vSGA7lXByTU/maxresdefault.jpg',
   summary: 'An empowering evening of prayer, spiritual renewal, and passionate pursuit of God’s presence.',
   transcriptStatus: 'Key message highlights and transcript outline available below.'
 }
 
 export const recentVideos = [
-  { id: '0H03yTCHnG0', slug: 'ummai-arathinkidnrom-live', title: 'Ummai Arathinkidnrom Live', speaker: 'Sam Joel', type: 'Worship', thumbnail: 'https://i.ytimg.com/vi/0H03yTCHnG0/hqdefault.jpg', date: 'May 10, 2026' },
-  { id: 's1JnsT4sJqY', slug: 'sunday-tamil-service', title: 'Sunday Tamil Service', speaker: 'Pastor Blesson Eliyesar', date: 'April 19, 2026', type: 'Sunday Service', thumbnail: 'https://i.ytimg.com/vi/s1JnsT4sJqY/hqdefault.jpg' },
-  { id: '9O6E_lhHxVc', slug: 'en-aathumave-live', title: 'En Aathumave Live', speaker: 'Peter Paul', type: 'Worship', thumbnail: 'https://i.ytimg.com/vi/9O6E_lhHxVc/hqdefault.jpg', date: 'March 29, 2026' },
-  { id: 'UxidJiHkaic', slug: 'uyarndhavarae-live', title: 'Uyarndhavarae Live', speaker: 'Peter Paul', type: 'Worship', thumbnail: 'https://i.ytimg.com/vi/UxidJiHkaic/hqdefault.jpg', date: 'March 15, 2026' },
-  { id: 'tONNfB82YKE', slug: 'vetri-sirantharae-live', title: 'Vetri Sirantharae Live', speaker: 'Peter Paul', type: 'Worship', thumbnail: 'https://i.ytimg.com/vi/tONNfB82YKE/hqdefault.jpg', date: 'March 01, 2026' }
+  { id: '0H03yTCHnG0', slug: 'ummai-arathinkidnrom-live', title: 'Ummai Arathinkidnrom Live', speaker: 'Sam Joel', type: 'Worship', thumbnail: 'https://i.ytimg.com/vi/0H03yTCHnG0/maxresdefault.jpg', date: 'May 10, 2026' },
+  { id: 's1JnsT4sJqY', slug: 'sunday-tamil-service', title: 'Sunday Tamil Service', speaker: 'Pastor Blesson Eliyesar', date: 'April 19, 2026', type: 'Sunday Service', thumbnail: 'https://i.ytimg.com/vi/s1JnsT4sJqY/maxresdefault.jpg' },
+  { id: '9O6E_lhHxVc', slug: 'en-aathumave-live', title: 'En Aathumave Live', speaker: 'Peter Paul', type: 'Worship', thumbnail: 'https://i.ytimg.com/vi/9O6E_lhHxVc/maxresdefault.jpg', date: 'March 29, 2026' },
+  { id: 'UxidJiHkaic', slug: 'uyarndhavarae-live', title: 'Uyarndhavarae Live', speaker: 'Peter Paul', type: 'Worship', thumbnail: 'https://i.ytimg.com/vi/UxidJiHkaic/maxresdefault.jpg', date: 'March 15, 2026' },
+  { id: 'tONNfB82YKE', slug: 'vetri-sirantharae-live', title: 'Vetri Sirantharae Live', speaker: 'Peter Paul', type: 'Worship', thumbnail: 'https://i.ytimg.com/vi/tONNfB82YKE/maxresdefault.jpg', date: 'March 01, 2026' }
 ]
 
 export const allSermons = [featuredSermon, ...recentVideos]
